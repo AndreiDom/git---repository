245493b104c243646b74d769068e982527837099
Doom <andreidomashkevich@mail.ru>
2015-10-26 23:56:58 +0300

    Решённая задача.

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ int main()
 {
 
 cout << "Заполните матрицу числами " << endl;
+
 for(int rowNum = 0; rowNum < MatrixSize; rowNum++){
     for(int columnNum = 0; columnNum < MatrixSize; columnNum++){
         cin >> ourMatrix[rowNum][columnNum];
     }
 }
+cout << endl;
 for(int rowNum = 0; rowNum < MatrixSize; rowNum++){
     cout << "|";
     for(int columnNum = 0; columnNum < MatrixSize; columnNum++){
@@ -25,7 +27,14 @@ for(int rowNum = 0; rowNum < MatrixSize; rowNum++){
         columnSum[rowNum] += ourMatrix[columnNum][rowNum];
     }
      cout << "|" << endl;
-
+     cout << endl;
+}
+for(int i = 0; i < MatrixSize; i++){
+    cout << "Сумма " << i+1 << " ряда "<< rowSum[i] << endl;
+}
+cout << endl;
+for(int i = 0; i < MatrixSize; i++){
+    cout << "Сумма " << i+1 << " столбца "<< columnSum[i] << endl;
 }
 
 	return 0;
